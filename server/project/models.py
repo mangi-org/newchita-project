23a55f624f2e92818237a47f43809ce93f0451d1
Alexsandr <alssval@gmail.com>
2023-09-05 14:22:21 +0300

    update(server): update dockerignore

--- server/project/models.py
+++ server/project/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class AboutProject(models.Model):
     title = models.CharField(max_length=100, null=False)
     description = models.TextField(null=False)
-    image = models.ImageField(upload_to='media/about')
+    image = models.ImageField(upload_to='about')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     latitude = models.FloatField(null=False)
