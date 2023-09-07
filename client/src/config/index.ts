727f949a0dcbcaf611adb528ec2f4b9d195c0700
Alexsandr <alssval@gmail.com>
2023-09-07 18:29:19 +0300

    fix

--- client/src/config/index.ts
+++ client/src/config/index.ts
@@ -1,5 +1,5 @@
 const getApiURL = (): string => {
-    const result = import.meta.env.VITE_API_URL + ":" + import.meta.env.VITE_API_PORT + "/api/v1/"
+    const result = import.meta.env.VITE_API_URL + "/api/v1/"
     const ssl = import.meta.env.VITE_SSL === "true" ? true : false
     if (ssl) {
         return "https://" + result
