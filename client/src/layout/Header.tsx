2741b204b077e123380278ee92db81124e6db145
Alexsandr <alssval@gmail.com>
2023-09-07 17:38:08 +0300

    fix

--- client/src/layout/Header.tsx
+++ client/src/layout/Header.tsx
@@ -35,7 +35,7 @@ function Header() {
                     </Link>
                     <div className="flex items-center lg:order-2">
                         {/* TODO write mail icon */}
-                        <Link to="#" className="bg-red-500 w-8 h-8 flex items-center justify-center">
+                        <Link to="mailto:info@elysiumclub.ru" className="bg-red-500 w-8 h-8 flex items-center justify-center">
                             <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="white" strokeWidth={2}>
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                             </svg>
