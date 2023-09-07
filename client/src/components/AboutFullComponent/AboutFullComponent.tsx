587f8c093a04425077df11eeffd35382b06cf0a1
Alexsandr <alssval@gmail.com>
2023-09-07 12:57:45 +0300

    update(full): fix bug

--- client/src/components/AboutFullComponent/AboutFullComponent.tsx
+++ client/src/components/AboutFullComponent/AboutFullComponent.tsx
@@ -1,56 +1,20 @@
-import { useEffect, useState } from "react"
 import { RootState } from "../../redux"
 import { useSelector } from "react-redux"
 import ReactMarcdown from "react-markdown"
-import { AboutProjectAPI } from "../../interface"
-import { getSingleAboutProject } from "../../axios"
-import { EventsComponent } from ".."
+import { useNavigate } from "react-router-dom"
+import { detectBgColor } from "../../utils"
 
 function AboutFullComponent() {
 
-    const stateDefault: AboutProjectAPI = {
-        id: -1,
-        title: "",
-        address: "",
-        description: "",
-        image: "",
-        createdAt: "",
-        updatedAt: "",
-        color: "",
-        latitude: 0,
-        longitude: 0
-    }
+    const navaigate = useNavigate()
 
     const data = useSelector((state: RootState) => state.project)
-    const [nextEvent, setNextEvent] = useState(stateDefault)
-    const [prevEvent, setPrevEvent] = useState(stateDefault)
 
-    useEffect(() => {
-        async function fetchData() {
-            try {
-                const next = await getSingleAboutProject(Number(data.id) + 1)
-                console.log(next)
-                if (next?.id) {
-                    setNextEvent(next)
-                }
-            } catch (e) {
-                console.log(e)
-            }
+    const BgColor = detectBgColor(String(data.color))
 
-            try {
-                const prev = await getSingleAboutProject(Number(data.id) - 1)
-                console.log(prev)
-                if (prev?.id) {
-                    setPrevEvent(prev)
-                }
-            } catch (e) {
-                console.log(e)
-            }
-
-        }
-        fetchData().then()
-    }, [data.id])
-
+    const handleClick = () => {
+        navaigate(-1)
+    }
 
     return (
         <>
@@ -61,13 +25,8 @@ function AboutFullComponent() {
                 <hr />
                 <p><ReactMarcdown>{data.description}</ReactMarcdown></p>
             </div>
-            <div className="flex flex-row">
-                {prevEvent.id !== -1 && <div className="float-left">
-                    <EventsComponent project={prevEvent} />
-                </div>}
-                {nextEvent.id !== -1 && <div className="float-right">
-                    <EventsComponent project={nextEvent} />
-                </div>}
+            <div className="flex flex-row mx-4">
+                <button onClick={handleClick} className={"px-4 h-8 text-white rounded self-center flex items-center justify-center " + BgColor}>Назад</button>
             </div>
         </>
     )
